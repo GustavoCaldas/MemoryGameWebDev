122d42834a745fa965a719e7f1c4bffc9b25e3c2
Gustavo Caldas de Souza <gustavocaldes@gmail.com>
2020-12-27 21:50:09 -0300

    commit JS

--- scripts/game.js
+++ scripts/game.js
@@ -1,3 +1,13 @@
+const FRONT = "card_front"
+const BACK = "card_back"
+
+let apps = ['bootstrap',
+    'css',
+    'electron',
+    'firebase',
+    'html',
+    'javascript',
+    'jquery']
 function restart(){
-    
+
 }
